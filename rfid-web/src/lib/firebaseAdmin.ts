764f580db1b8d8f2154361938361c96066b6dd9b
Elchocazo <moz658@gmail.com>
2026-07-15 20:24:47 -0500

    Fix next build crashing due to missing env vars

--- rfid-web/src/lib/firebaseAdmin.ts
+++ rfid-web/src/lib/firebaseAdmin.ts
@@ -4,13 +4,17 @@ import { getAuth } from 'firebase-admin/auth';
 
 if (!getApps().length) {
   try {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
+    if (process.env.FIREBASE_PRIVATE_KEY) {
+      initializeApp({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        }),
+      });
+    } else {
+      initializeApp({ projectId: 'demo-project' });
+    }
   } catch (error) {
     console.error('Firebase admin initialization error', error);
   }
